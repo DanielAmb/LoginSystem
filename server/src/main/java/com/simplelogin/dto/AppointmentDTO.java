82a54ba0e128d876abab008f4a18f0abbdf9fb56
Daniel Ambrose <103330514+DanielAmb@users.noreply.github.com>
2025-08-03 11:53:29 -0400

    Update AppointmentDTO.java

--- server/src/main/java/com/simplelogin/dto/AppointmentDTO.java
+++ server/src/main/java/com/simplelogin/dto/AppointmentDTO.java
@@ -6,9 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data // Generates getters, setters, toString(), equals(), and hashCode()
-@NoArgsConstructor // Generates a no-args constructor (required for JSON deserialization)
-@AllArgsConstructor // Generates a constructor with all fields (optional but useful)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor 
 public class AppointmentDTO {
     private String title;
     private String description;
